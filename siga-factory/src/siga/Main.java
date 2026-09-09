80c52b8a71798face45fbcb1b291dcc79468b830
Katllyn Rezende <Katllynrezende@gmail.com>
2026-09-09 17:22:46 -0300

    Atualização da "Main" para agora exibir o novo painel, da Secretaria. Também atualização do titulo principal do painel

--- siga-factory/src/siga/Main.java
+++ siga-factory/src/siga/Main.java
@@ -2,7 +2,7 @@ package siga;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "=== SIGA - Atividade Factory (Código Final) ===" );
+        System.out.println( "=== SIGA - Atividade Factory Method (Código Final) ===" );
         GerenciadorLogin login = new GerenciadorLogin();
         login.montarPainel(new CriadorPainelAluno());
         System.out.println();
@@ -10,6 +10,7 @@ public class Main {
         System.out.println();
         login.montarPainel(new CriadorPainelCoordenador());
         System.out.println();
+        login.montarPainel(new CriadorPainelSecretaria());
     }
     
 }
