6449c66fa353f26da7c4d48fede3b4fa91759628
Katllyn Rezende <Katllynrezende@gmail.com>
2026-09-09 16:39:47 -0300

    Atualização de Main para usar o Factory Method

--- siga-factory/src/siga/Main.java
+++ siga-factory/src/siga/Main.java
@@ -1,26 +1,15 @@
 package siga;
 
-/**
- * Ponto de entrada do SIGA (código INICIAL da atividade da Aula 5).
- *
- * Demonstra, em execução, a criação de painéis por perfil. O programa
- * FUNCIONA — mas a classe GerenciadorLogin precisa ser modificada a cada
- * novo perfil. Sua tarefa é encapsular essa criação com a Simple Factory e,
- * depois, com o padrão Factory Method.
- */
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("=== SIGA - Atividade Factory (código intermediário) ===\n");
-
+        System.out.println( "=== SIGA - Atividade Factory (Código Final) ===" );
         GerenciadorLogin login = new GerenciadorLogin();
-
-        // Simulação de logins de diferentes perfis.
-        login.montarPainel("ALUNO");
+        login.montarPainel(new CriadorPainelAluno());
         System.out.println();
-        login.montarPainel("PROFESSOR");
+        login.montarPainel(new CriadorPainelProfessor());
         System.out.println();
-        login.montarPainel("COORDENADOR");
-
+        login.montarPainel(new CriadorPainelCoordenador());
+        System.out.println();
     }
+    
 }
